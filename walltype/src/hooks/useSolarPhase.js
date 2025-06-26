a8bb674435cfaa614a530bd7943397aa542479f1
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-06-26 23:14:49 +0530

    typo

--- walltype/src/hooks/useSolarPhase.js
+++ walltype/src/hooks/useSolarPhase.js
@@ -1,37 +1,36 @@
-/* eslint-disable no-undef */
-import { useState,useEffect } from "react";
-import SunCalc from 'suncalc'
+import { useState, useEffect } from "react";
+import SunCalc from 'suncalc';
 import getPhaseFromTime from '../utils/getPhaseFromTime';
 
-export default function useSolarPhase(){
-    const[phase,setPhase]=useState('day');
-    useEffect(()=>{
-        isMounted=true;
+export default function useSolarPhase() {
+    const [phase, setPhase] = useState('day');
+    useEffect(() => {
+        let isMounted = true;
         navigator.geolocation.getCurrentPosition(
-            ({coords})=>{
-                const{latitude,longitude}=coords;
-                const times=SunCalc.getTimes(newDate(),latitude,longitude);
-                const tms={
-                    dawn:times.nauticalDawn.getTime(),
-                    sunrise:times.sunrise.getTime(),
-                    sunset:times.sunset.getTime(),
-                    dusk:times.nauticalDusk.getTime()
+            ({ coords }) => {
+                const { latitude, longitude } = coords;
+                const times = SunCalc.getTimes(new Date(), latitude, longitude);
+                const tms = {
+                    dawn: times.nauticalDawn.getTime(),
+                    sunrise: times.sunrise.getTime(),
+                    sunset: times.sunset.getTime(),
+                    dusk: times.nauticalDusk.getTime()
                 };
-                function update(){
-                    if(isMounted){
-                        setPhase(getPhaseFromTime(tms,newDate()));
+                function update() {
+                    if (isMounted) {
+                        setPhase(getPhaseFromTime(tms, new Date()));
                     }
                 }
                 update();
-                const id=setInterval(update,60*1000);
-                return()=>{
+                const id = setInterval(update, 60 * 1000);
+                return () => {
                     clearInterval(id);
-                    isMounted=false;
+                    isMounted = false;
                 };
             },
-            (err)=>console.error('Geolocation error:',err),
-            {enableHighAccuracy:false}
+            (err) => console.error('Geolocation error:', err),
+            { enableHighAccuracy: false }
         );
-    },[]);
+    }, []);
     return phase;
 }
