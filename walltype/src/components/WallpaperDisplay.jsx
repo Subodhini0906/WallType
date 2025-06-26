d4b77d74d971307123063411b52a887edf82d1d1
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-06-26 23:01:43 +0530

    wD added

--- walltype/src/components/WallpaperDisplay.jsx
+++ walltype/src/components/WallpaperDisplay.jsx
@@ -1,12 +1,16 @@
-export default function WallpaperDispay({phase}){
-    const WallpaperMap={
-        sunrise:'/day.jpg',
-        day:'/Day1.jpg',
-        sunset:'/day2.jpg',
-        night: '/night.jpg',
+export default function WallpaperDisplay({ image, phase }) {
+    const filters = {
+      dawn: 'brightness-90 contrast-110',
+      day: 'brightness-100',
+      golden: 'brightness-105 hue-rotate-5',
+      dusk: 'brightness-95 contrast-105 invert-5',
+      night: 'brightness-80 contrast-90 saturate-80'
     };
-
-    return(
-        <div className="h-screen w-screen bg-cover bg-center" style={{background:`url(${WallpaperMap[phase]})`}}/>
+  
+    return (
+      <div
+        className={`absolute inset-0 bg-cover bg-center ${filters[phase]}`}
+        style={{ backgroundImage: image ? `url(${image})` : 'none' }}
+      />
     );
-}
+  }
